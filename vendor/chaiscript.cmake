dfa2085b6a707ba2d0b7dcd1ae191bf46e0a46e4
pbentes <pedrogomes2000.3@gmail.com>
2024-09-01 21:46:19 +0100

    Removed chaiscript

--- vendor/chaiscript.cmake
+++
@@ -1,8 +0,0 @@
-cmake_minimum_required(VERSION 3.16)
-include(FetchContent)
-
-FetchContent_Declare(
-  chai
-  GIT_REPOSITORY https://github.com/ChaiScript/ChaiScript.git
-)
-FetchContent_MakeAvailable(chai)
