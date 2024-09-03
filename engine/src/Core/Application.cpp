67a36e7ddbb1e3c3d87aa8ceb47b5621f33e3542
pbentes <pedrogomes2000.3@gmail.com>
2024-09-03 22:24:01 +0100

    Added instrumentor

--- engine/src/Core/Application.cpp
+++ engine/src/Core/Application.cpp
@@ -1,9 +1,12 @@
 #include "Application.h"
+#include "../Debug/Instrumentor.h"
 
 #include <raylib.h>
 
 namespace Engine {
     Application::Application(const char* window_title) {
+        Instrumentor::Get().BeginSession("Engine");
+
         const int screenWidth = 800;
         const int screenHeight = 450;
 
@@ -12,11 +15,14 @@ namespace Engine {
 
     Application::~Application() {
         CloseWindow();
+        Instrumentor::Get().EndSession();
     }
 
     void Application::run() {
+        PROFILE_SCOPE("Run Function");
         while (!WindowShouldClose())
         {
+            PROFILE_SCOPE("Main Loop");
             BeginDrawing();
                 ClearBackground(Color {90 , 99 , 156, 255});
                 DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
