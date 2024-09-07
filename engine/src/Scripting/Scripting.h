940bf7cb24654e7873dd61c5b8af000340475862
pbentes <pedrogomes2000.3@gmail.com>
2024-09-07 21:45:36 +0100

    Defined scripting behaviour class

--- engine/src/Scripting/Scripting.h
+++ engine/src/Scripting/Scripting.h
@@ -19,4 +19,20 @@ namespace Engine {
         private:
             std::shared_ptr<entt::registry> m_Registry;
     };
+
+    class ScriptingBehaviour {
+        public:
+            using delta_type = std::double_t;
+
+            ScriptingBehaviour() = default;
+            ~ScriptingBehaviour() = default;
+
+            virtual void init() = 0; // Called when adding the script to the scripting process
+            virtual void exit() = 0; // Called when removing the script to the scripting process
+            virtual void update(delta_type delta_time) = 0; // Called every frame before rendering starts
+            virtual void late_update(delta_type delta_time) = 0; // Called every frame after rendering ends
+            virtual void fixed_update(delta_type delta_time) = 0; // Called every physics frame before processing physics
+            virtual void fixed_late_update(delta_type delta_time) = 0; // Called every physics frame after processing physics
+            virtual void on_gui(delta_type delta_time) = 0; // Called every frame, used to draw gui elements
+    };
 }
