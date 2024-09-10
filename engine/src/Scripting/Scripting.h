2e767704e62859c58de0a3526d4f344e68d27f85
pbentes <pedrogomes2000.3@gmail.com>
2024-09-10 20:27:37 +0100

    Moved behaviour component into its own file

--- engine/src/Scripting/Scripting.h
+++ engine/src/Scripting/Scripting.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "sol/forward.hpp"
 #include <entt/entt.hpp>
 #include <sol/sol.hpp>
 
@@ -21,21 +22,5 @@ namespace Engine {
         private:
             sol::state m_Lua;
             std::shared_ptr<entt::registry> m_Registry;
-    };
-
-    class ScriptingBehaviour {
-        public:
-            using delta_type = std::double_t;
-
-            ScriptingBehaviour() = default;
-            ~ScriptingBehaviour() = default;
-
-            virtual void init() = 0; // Called when adding the script to the scripting process
-            virtual void exit() = 0; // Called when removing the script to the scripting process
-            virtual void update(delta_type delta_time) = 0; // Called every frame before rendering starts
-            virtual void late_update(delta_type delta_time) = 0; // Called every frame after rendering ends
-            virtual void fixed_update(delta_type delta_time) = 0; // Called every physics frame before processing physics
-            virtual void fixed_late_update(delta_type delta_time) = 0; // Called every physics frame after processing physics
-            virtual void on_gui(delta_type delta_time) = 0; // Called every frame, used to draw gui elements
     };
 }
