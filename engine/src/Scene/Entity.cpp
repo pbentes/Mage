5d40926f270a3f03a029606663ace0ae97750588
pbentes <pedrogomes2000.3@gmail.com>
2024-09-12 20:48:09 +0100

    Implemented entity class

---
+++ engine/src/Scene/Entity.cpp
@@ -0,0 +1,59 @@
+#include "Entity.h"
+#include "Scene.h"
+
+namespace Engine {
+    template<typename T, typename... Args>
+	T& Entity::AddComponent(Args&&... args) {
+        return m_Scene->m_Registry.emplace<T>(m_EntityHandle, std::forward<Args>(args)...);
+    }
+
+    template<typename T>
+	T& Entity::GetComponent() {
+        return m_Scene->m_Registry.get<T>(m_EntityHandle);
+    }
+
+    template<typename T>
+	const T& Entity::GetComponent() const {
+        return m_Scene->m_Registry.get<T>(m_EntityHandle);
+    }
+
+    template<typename... T>
+    bool Entity::HasComponent() {
+        return m_Scene->m_Registry.all_of<T...>(m_EntityHandle);
+    }
+
+    template<typename... T>
+    bool Entity::HasComponent() const {
+        return m_Scene->m_Registry.all_of<T...>(m_EntityHandle);
+    }
+
+    template<typename...T>
+    bool Entity::HasAny() {
+        return m_Scene->m_Registry.any_of<T...>(m_EntityHandle);
+    }
+
+    template<typename...T>
+    bool Entity::HasAny() const {
+        return m_Scene->m_Registry.any_of<T...>(m_EntityHandle);
+    }
+
+    template<typename T>
+    void Entity::RemoveComponent() {
+        m_Scene->m_Registry.remove<T>(m_EntityHandle);
+    }
+
+    template<typename T>
+    void Entity::RemoveComponentIfExists() {
+        if (!HasComponent<T>())
+            return;
+        m_Scene->m_Registry.remove<T>(m_EntityHandle);
+    }
+
+    bool Entity::IsValid() const { 
+        return (m_EntityHandle != entt::null) && m_Scene && m_Scene->m_Registry.valid(m_EntityHandle); 
+    }
+    
+	Entity::operator bool() const { 
+        return IsValid(); 
+    }
+}
---
+++ engine/src/Scene/Entity.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "../Components/Tag.h"
+
+#include <entt/entt.hpp>
+
+namespace Engine {
+    class Scene;
+
+    class Entity {
+        public:
+            Entity() = default;
+            Entity(entt::entity handle, Scene* scene): m_EntityHandle(handle), m_Scene(scene) {}
+            ~Entity() = default;
+            
+            template<typename T, typename... Args>
+		    T& AddComponent(Args&&... args);
+
+            template<typename T>
+            T& GetComponent();
+
+            template<typename T>
+            const T& GetComponent() const;
+
+            template<typename... T>
+            bool HasComponent();
+
+            template<typename... T>
+            bool HasComponent() const;
+
+            template<typename...T>
+            bool HasAny();
+
+            template<typename...T>
+            bool HasAny() const;
+
+            template<typename T>
+            void RemoveComponent();
+
+            template<typename T>
+            void RemoveComponentIfExists();
+
+            std::string& Name() { return HasComponent<TagComponent>() ? GetComponent<TagComponent>().tag : DefaultName; }
+            const std::string& Name() const { return HasComponent<TagComponent>() ? GetComponent<TagComponent>().tag : DefaultName; }
+            bool IsValid() const;
+
+
+            operator uint32_t () const { return (uint32_t)m_EntityHandle; }
+            operator entt::entity () const { return m_EntityHandle; }
+            operator bool () const;
+        private:
+            entt::entity m_EntityHandle { entt::null };
+            Scene* m_Scene = nullptr;
+            inline static std::string DefaultName = "Unnamed";
+    };
+}
