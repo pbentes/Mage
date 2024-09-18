e73417a213f44153c0687a7f19a67d7c75fed7d4
pbentes <pedrogomes2000.3@gmail.com>
2024-09-18 23:32:11 +0100

    Added added a simple comment

--- engine/src/Components/SceneComponent.h
+++ engine/src/Components/SceneComponent.h
@@ -3,7 +3,8 @@
 #include "../Core/UUID.h"
 
 namespace Engine {
+    // The scene component is used to embedd scenes within one another
     typedef struct SceneComponent {
         UUID64 SceneID;
     } SceneComponent;
-}
+} 
