7a010d6fdcdd09305c28eb572ef7744a6b688683
pbentes <pedrogomes2000.3@gmail.com>
2024-09-30 23:39:18 +0100

    As I rewrite the engine parts these tests were deleted

--- tests/src/SceneTests.cpp
+++ tests/src/SceneTests.cpp
@@ -1,18 +1,7 @@
 #include <gtest/gtest.h>
 
-#include <Scene/Scene.h>
-#include <Scene/Entity.h>
-
-#include <Components/UUID.h>
-#include <Components/Transform.h>
-#include <Components/Tag.h>
-#include <Components/Node.h>
-
 namespace EngineTests {
     TEST(Scene, CreateNewScene) {
-        // Arrange
-        Engine::Scene scene("test scene", false);
-
         EXPECT_STRNE("hello", "world");
         EXPECT_EQ(7 * 6, 42);
     }
