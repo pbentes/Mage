432e12f47f7c4661c2a49ad3c3ea1096985cc6a2
pbentes <pedrogomes2000.3@gmail.com>
2024-10-08 21:12:33 +0100

    Updated runtime to handle project paths

--- runtime/src/main.cpp
+++ runtime/src/main.cpp
@@ -1,6 +1,21 @@
+#include <Assets/AssetDatabase.h>
 #include <Core/Application.h>
 
-int main() {
+#include <filesystem>
+#include <iostream>
+#include <vector>
+
+int main(int argc, char* argv[]) {
+    std::string project_path;
+
+    if (argc < 1)
+        project_path = std::filesystem::current_path().remove_filename().string() + "/project";
+    else
+        project_path = std::string(argv[1]);
+
+    std::cout << project_path << std::endl;
+
+    Engine::AssetDatabase asset_db(std::vector<std::string> { project_path });
     Engine::Application app;
     app.run();
 }
