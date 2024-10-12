6703b96771d3c82d8e523e1c7ee0c839e3c06c79
pbentes <pedrogomes2000.3@gmail.com>
2024-10-12 15:05:03 +0100

    Moved assert to the logger

--- engine/src/Debug/Logger.h
+++ engine/src/Debug/Logger.h
@@ -31,6 +31,18 @@
     #define INFO(...)
 #endif
 
+#ifndef NDEBUG
+    #define ENABLE_ASSERTS
+#endif
+
+#ifdef ENABLE_ASSERTS
+    #define ENGINE_ASSERT(condition, ...) ::Engine::Logger::GetInstance().Assert(condition, "ENGINE_ASSERTION_FAILED", ##__VA_ARGS__)
+    #define ASSERT(condition, ...) ::Engine::Logger::GetInstance().Assert(condition, "ASSERTION_FAILED", ##__VA_ARGS__)
+#else
+    #define ENGINE_ASSERT(condition, ...) ((void)0)
+    #define ASSERT(condition, ...) ((void)0)
+#endif
+
 namespace Engine {
     class Logger {
         public:
