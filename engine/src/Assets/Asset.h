151663ba4f37ca92eaa26515edfbb2d84c05c137
pbentes <pedrogomes2000.3@gmail.com>
2024-10-12 14:25:55 +0100

    Started simplifying the asset class

--- engine/src/Assets/Asset.h
+++ engine/src/Assets/Asset.h
@@ -1,9 +1,7 @@
 #pragma once
 
 #include <cstddef>
-#include <memory>
 #include <string>
-#include <vector>
 
 namespace Engine {
     class Asset {
@@ -11,19 +9,13 @@ namespace Engine {
             Asset(size_t id, std::string path, std::string directory, std::string extension, bool deleted);
             ~Asset() = default;
 
-            void LoadAssetSync();
-            void LoadAssetAsync();
-            void StreamAsset(size_t startByte, size_t endByte);
-            void UnloadAsset();
-
             std::string ToString() const;
+            std::string GetPath();
         private:
             size_t m_Id;
             std::string m_Directory;
             std::string m_Extension;
             bool m_Deleted;
-        public:
-            std::shared_ptr<std::vector<std::byte>> m_Blob;
             std::string m_Path;
     };
 }
