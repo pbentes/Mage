4fd6a182d11117c61d41147e2cf5048cec6386a0
pbentes <pedrogomes2000.3@gmail.com>
2024-10-12 12:40:36 +0100

    Added annotations to the behaviour class

--- tests/project/library/behaviour.lua
+++ tests/project/library/behaviour.lua
@@ -15,52 +15,70 @@ end
 
 -- Lifecycle Hooks
 
+---@return nil
 function Behaviour:start()
 end
 
+---@param delta number?
+---@return nil
 function Behaviour:update(delta)
 end
 
+---@param delta number?
+---@return nil
 function Behaviour:late_update(delta)
 end
 
+---@return nil
 function Behaviour:on_destroy()
 end
 
+---@return nil
 function Behaviour:on_enable()
 end
 
+---@return nil
 function Behaviour:on_disable()
 end
 
 -- Physics Hooks
 
+---@param delta number?
+---@return nil
 function Behaviour:fixed_update(delta)
 end
 
+---@return nil
 function Behaviour:on_trigger_enter()
 end
 
+---@return nil
 function Behaviour:on_trigger_stay()
 end
 
+---@return nil
 function Behaviour:on_trigger_exit()
 end
 
+---@return nil
 function Behaviour:on_collision_enter()
 end
 
+---@return nil
 function Behaviour:on_collision_stay()
 end
 
+---@return nil
 function Behaviour:on_collision_exit()
 end
 
 -- Rendering hooks
 
+---@return nil
 function Behaviour:on_draw_gizmos()
 end
 
+---@return nil
 function Behaviour:on_gui()
 end
 
