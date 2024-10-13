822282c1a70cb02a8a0b4a0bcf72c7f8042b44c3
pbentes <pedrogomes2000.3@gmail.com>
2024-10-13 18:42:26 +0100

    Updated behaviour tests to reflect changes

--- tests/src/BehaviourTests.cpp
+++ tests/src/BehaviourTests.cpp
@@ -1,3 +1,4 @@
+#include "Core/UUID.h"
 #include <gtest/gtest.h>
 
 #include <Scripting/Behaviour.h>
@@ -7,9 +8,25 @@ namespace EngineTests {
     TEST(Behaviour, CreateBehaviour) {
         Engine::Scripting scripting;
 
-        scripting.LoadScript("C:\\Users\\Pedro Bentes\\Desktop\\Mage\\tests\\project\\library\\behaviour.lua");
-        scripting.LoadScript("C:\\Users\\Pedro Bentes\\Desktop\\Mage\\tests\\project\\assets\\scenes\\script.lua");
+        scripting.LoadLibrary("C:\\Users\\Pedro Bentes\\Desktop\\Mage\\tests\\project\\library\\behaviour.lua");
+        scripting.LoadUserScript("C:\\Users\\Pedro Bentes\\Desktop\\Mage\\tests\\project\\assets\\scenes\\script.lua");
 
+        uint64_t entity = 1;
+        uint64_t entity2 = 3;
+
+        // 1_Player
+        std::unique_ptr<Engine::Behaviour> bhvr = scripting.AttachBehaviourToEntity(Engine::UUID64(entity), "C:\\Users\\Pedro Bentes\\Desktop\\Mage\\tests\\project\\assets\\scenes\\script.lua");
+        std::unique_ptr<Engine::Behaviour> bhvr2 = scripting.AttachBehaviourToEntity(Engine::UUID64(entity2), "C:\\Users\\Pedro Bentes\\Desktop\\Mage\\tests\\project\\assets\\scenes\\script.lua");
+
+        bhvr->update(1);
+        bhvr->update(1);
+        bhvr2->update(1);
+
+        sol::table test = scripting.m_Lua.get<sol::table>("Entity1_Player");
+        int testi = test.get<int>("j");
+
+        std::cout << "Test: " << testi << std::endl;
+        
         /*EXPECT_EQ(10, behaviour.self.get<int>("j"));
 
         behaviour.update(0.0f);
