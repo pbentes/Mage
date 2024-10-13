46b92401caf6c45f579cbf031089048a6d04e0a7
pbentes <pedrogomes2000.3@gmail.com>
2024-10-14 00:13:51 +0100

    Added type annotations to settings

--- tests/project/library/settings.lua
+++ tests/project/library/settings.lua
@@ -1,36 +1,65 @@
 Settings = {
+    --- @type table
     config = {
+        --- @type string
         name = "",
+
+        --- @type table
         name_localized = {},
+
+        --- @type string
         version = "",
+
+        --- @type string
         icon = ""
     },
-    gravity = 9.8,
+
+    --- @type table
     run = {
+        --- @type string
         main_scene = ""
     },
+
+    --- @type table
     audio = {
+        --- @type string
         default_audio_bus = ""
     },
+
+    --- @type table
     physics = {
+        --- @type table
         common = {
+            --- @type integer
             physics_ticks_per_second = 60,
+
+            --- @type integer
             max_physics_steps_per_frame = 8
         },
+
+        --- @type table
         _3d = {
+            --- @type number
             default_gravity = 9.8,
-            default_gravity_vector = { x = 0, y = -1, z = 0 }
+
+            --- @type Vector3
+            default_gravity_vector = Vector3.Down
         },
+
+        --- @type table
         _2d = {
+            --- @type number
             default_gravity = 980,
-            default_gravity_vector = { x = 0, y = 1 }
+
+            --- @type Vector2
+            default_gravity_vector = Vector2.Up
         }
     }
 }
 
 ---Get a the value related to a given key
----@param key string
----@return (string | number)
+--- @param key string
+--- @return (string | number | integer)
 function Settings:get(key)
     return self:get(key)
 end
