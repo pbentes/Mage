53432ff17e24b534be6e7f183822df5d12a525b5
pbentes <pedrogomes2000.3@gmail.com>
2024-12-16 22:01:52 +0000

    Updated window interface

--- engine/src/interfaces/window.h
+++ engine/src/interfaces/window.h
@@ -5,31 +5,31 @@
 typedef struct Window Window;
 
 typedef struct WindowAPI {
-    Window (*create)(int, int, std::string);
-    void   (*close)(Window*);
-    bool   (*should_close)(Window*);
-    bool   (*is_ready)(Window*);
-    bool   (*is_fullscreen)(Window*);
-    bool   (*is_hidden)(Window*);
-    bool   (*is_minimized)(Window*);
-    bool   (*is_maximized)(Window*);
-    bool   (*is_focused)(Window*);
-    bool   (*is_resized)(Window*);
-    void   (*toggle_fullscreen)(Window*);
-    void   (*toggle_borderless)(Window*);
-    void   (*maximize)(Window*);
-    void   (*minimize)(Window*);
-    void   (*restore)(Window*);
-    void   (*set_icon)(Window*, void*); // Todo: Create an Image type
-    void   (*set_title)(Window*, std::string);
-    void   (*set_position)(int x, int y);
-    void   (*set_monitor)(int monitor);
-    void   (*set_minSize)(int width, int height);
-    void   (*set_maxSize)(int width, int height);
-    void   (*set_size)(int width, int height);
-    void   (*set_opacity)(float opacity);
-    void   (*set_focused)(void);
-    void*  (*get_handle)(void);
+    Window (*create)(int width, int height, std::string title);
+    void   (*close)(Window* window);
+    bool   (*should_close)(Window* window);
+    bool   (*is_ready)(Window* window);
+    bool   (*is_fullscreen)(Window* window);
+    bool   (*is_hidden)(Window* window);
+    bool   (*is_minimized)(Window* window);
+    bool   (*is_maximized)(Window* window);
+    bool   (*is_focused)(Window* window);
+    bool   (*is_resized)(Window* window);
+    void   (*toggle_fullscreen)(Window* window);
+    void   (*toggle_borderless)(Window* window);
+    void   (*maximize)(Window* window);
+    void   (*minimize)(Window* window);
+    void   (*restore)(Window* window);
+    void   (*set_icon)(Window* window, void* image); // Todo: Create an Image type
+    void   (*set_title)(Window* window, std::string title);
+    void   (*set_position)(Window* window, int x, int y);
+    void   (*set_monitor)(Window* window, int monitor);
+    void   (*set_min_size)(Window* window, int width, int height);
+    void   (*set_max_size)(Window* window, int width, int height);
+    void   (*set_size)(Window* window, int width, int height);
+    void   (*set_opacity)(Window* window, float opacity);
+    void   (*set_focused)(Window* window);
+    void*  (*get_handle)(Window* window);
 } WindowAPI;
 
 #include "../core/window.h"
