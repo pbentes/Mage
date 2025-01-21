8e951a7b53f4c1208f0bd7eeac8ba40d2b417d6d
pbentes <pedrogomes2000.3@gmail.com>
2025-01-21 23:42:35 +0000

    Updated logger to remove classes

---
+++ engine/src/debug/logger.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <chrono>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+
+#define LOGGER_OUTPUT_COUT
+
+#ifndef DO_NOT_OUTPUT_ERRORS
+    #define ERROR(...) Error(__VA_ARGS__)
+#else 
+    #define ERROR(...)
+#endif
+
+#ifndef DO_NOT_OUTPUT_DEBUG
+    #define DEBUG(...) Debug(__VA_ARGS__)
+#else
+    #define DEBUG(...)
+#endif
+
+#ifndef DO_NOT_OUTPUT_INFO
+    #define INFO(...)  Info(__VA_ARGS__)
+#else
+    #define INFO(...)
+#endif
+
+template<typename... Args>
+void Log(const char* mode, Args&&... args) {
+    std::ofstream m_OutputStream;
+    m_OutputStream.open("engine.log", std::fstream::app);
+
+    std::ostringstream oss;
+
+    auto now = std::chrono::system_clock::now();
+    std::time_t now_time_t = std::chrono::system_clock::to_time_t(now);
+    struct tm now_tm;
+    localtime_s(&now_tm, &now_time_t);
+
+    oss << "[" << std::put_time(&now_tm, "%Y-%m-%d %H:%M:%S") << "] ";
+    oss << "[" << mode << "] ";
+    (oss << ... << std::forward<Args>(args)) << std::endl;
+
+    m_OutputStream << oss.str();
+#ifdef LOGGER_OUTPUT_COUT
+    std::cout      << oss.str();
+#endif
+    m_OutputStream.flush();
+    m_OutputStream.close();
+}
+
+template<typename... Args>
+void Error(Args&&... args) {
+    Log("ERROR", std::forward<Args>(args)...);
+}
+
+template<typename... Args>
+void Debug(Args&&... args) {
+    Log("DEBUG", std::forward<Args>(args)...);
+}
+
+template<typename... Args>
+void Info(Args&&... args) {
+    Log("INFO", std::forward<Args>(args)...);
+}
