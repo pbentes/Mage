ce9a7ad6c5462e81e52948842deaaf9695922f67
pbentes <pedrogomes2000.3@gmail.com>
2025-01-22 13:05:26 +0000

    Added uuid utility

---
+++ engine/src/core/uuid.cpp
@@ -0,0 +1,23 @@
+#include "uuid.h"
+
+#include <random>
+
+static std::random_device random_device;
+
+static std::mt19937_64 eng64(random_device());
+static std::uniform_int_distribution<uint64_t> uniform_distribution64;
+
+static std::mt19937 eng32(random_device());
+static std::uniform_int_distribution<uint32_t> uniform_distribution32;
+
+uuid64 create_uuid64() {
+    return uuid64 {
+        .uuid = uniform_distribution64(eng64)
+    };
+}
+
+uuid32 create_uuid32() {
+    return uuid32 {
+        .uuid = uniform_distribution32(eng32)
+    };
+}
---
+++ engine/src/core/uuid.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <cstdint>
+
+struct uuid64 {
+    uint64_t uuid;
+
+    operator uint64_t () { return uuid; }
+    operator const uint64_t () const { return uuid; }
+};
+
+uuid64 create_uuid64();
+
+struct uuid32 {
+    uint32_t uuid;
+
+    operator uint32_t () { return uuid; }
+    operator const uint32_t () const { return uuid; }
+};
+
+uuid32 create_uuid32();
