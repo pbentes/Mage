665fc8d4f27cfe78cbf29f07a145d4512c712bb8
pbentes <pedrogomes2000.3@gmail.com>
2025-01-22 19:26:46 +0000

    Made the structs types

--- engine/src/core/uuid.h
+++ engine/src/core/uuid.h
@@ -2,20 +2,20 @@
 
 #include <cstdint>
 
-struct uuid64 {
+typedef struct uuid64 {
     uint64_t uuid;
 
     operator uint64_t () { return uuid; }
     operator const uint64_t () const { return uuid; }
-};
+} uuid64;
 
 uuid64 create_uuid64();
 
-struct uuid32 {
+typedef struct uuid32 {
     uint32_t uuid;
 
     operator uint32_t () { return uuid; }
     operator const uint32_t () const { return uuid; }
-};
+} uuid32;
 
 uuid32 create_uuid32();
