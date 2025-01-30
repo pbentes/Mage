d0cb1b37bf88ef935cf7e1c185686134fa063e51
pbentes <pedrogomes2000.3@gmail.com>
2025-01-30 21:16:25 +0000

    Behaviour is not an interface

--- engine/src/scripting/behaviour.hpp
+++ engine/src/scripting/behaviour.hpp
@@ -2,7 +2,7 @@
 
 #include "../core/reflection.hpp"
 
-typedef struct BehaviourAPI {
+typedef struct Behaviour {
     const char* stringname;
 
     void* data;
@@ -11,5 +11,6 @@ typedef struct BehaviourAPI {
     void (*init)(void);
     void (*update)(float);
     void (*late_update)(float);
+    void (*on_render)(void);
     void (*exit)(void);
-} BehaviourAPI;
+} Behaviour;
